b2962da7db499b622fab424bbe9768c45934c8f5
armenkahriman-max <armenkahriman93@gmail.com>
2026-07-07 11:39:35 +0200

    add funny book seeder

--- BookTracker.Api/Program.cs
+++ BookTracker.Api/Program.cs
@@ -7,6 +7,7 @@ using BookTracker.Api.Application.GetBookById;
 using BookTracker.Api.Application.CreateBook;
 using BookTracker.Api.Application.UpdateBook;
 using BookTracker.Api.Application.DeleteBook;
+using BookTracker.Api.Seeding;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,9 +34,11 @@ if (app.Environment.IsDevelopment())
 {
     using (var scope = app.Services.CreateScope())
     {
-        scope.ServiceProvider
-            .GetRequiredService<AppDbContext>()
-            .Database.EnsureCreated();
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        dbContext.Database.EnsureCreated();
+        if (builder.Configuration.GetValue<bool>("SeedDatabase"))
+            DatabaseSeeder.SeedBooks(dbContext, 500);
     }
 }
 

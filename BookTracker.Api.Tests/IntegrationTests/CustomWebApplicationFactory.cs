5be5d087f54b2bfe185b91d1468a4052f129db05
armenkahriman-max <armenkahriman93@gmail.com>
2026-07-22 11:40:41 +0200

    fix integration test disposal

--- BookTracker.Api.Tests/IntegrationTests/CustomWebApplicationFactory.cs
+++ BookTracker.Api.Tests/IntegrationTests/CustomWebApplicationFactory.cs
@@ -11,7 +11,7 @@ namespace BookTracker.Api.Tests.IntegrationTests;
 
 public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 {
-    private SqliteConnection connection = null!;
+    private SqliteConnection? connection;
 
     private static readonly KeyValuePair<string, string?>[] TestSettings =
     [
@@ -66,8 +66,12 @@ public class CustomWebApplicationFactory : WebApplicationFactory<Program>
 
     protected override void Dispose(bool disposing)
     {
+        if (disposing)
+        {
+            connection?.Dispose();
+        }
+
         base.Dispose(disposing);
-        connection.Dispose();
     }
 
     public EfReader GetReader() => new(Services);

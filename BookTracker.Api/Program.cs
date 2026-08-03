59044665b89d333b13f3b196afee90d715d99f99
armenkahriman-max <armenkahriman93@gmail.com>
2026-08-03 11:24:29 +0200

    run book tracker with docker compose

--- BookTracker.Api/Program.cs
+++ BookTracker.Api/Program.cs
@@ -5,11 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddBookTracker();
 
 // CORS - Only for development
+var frontendOrigin = builder.Configuration["FrontendOrigin"]
+?? "http://localhost:5173";
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.
+        WithOrigins(frontendOrigin)
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
